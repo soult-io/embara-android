5b939b66eb3a5d726dafe840e283cecccf263a68
pai <pai@stabpablo.eu>
2026-04-26 17:24:56 +0200

    Bump version to 1.0.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "eu.stabpablo.trek"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
