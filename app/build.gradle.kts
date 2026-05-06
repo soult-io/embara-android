24275cd595590d2153fbcc74e5f025db19adae96
pai <pai@stabpablo.eu>
2026-05-07 00:00:53 +0200

    v1.2.0: new branding icon, R8 minification, updated AndroidX deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.soult.embara"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.1"
+        versionCode = 7
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
